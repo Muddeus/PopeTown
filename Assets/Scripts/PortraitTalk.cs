e6dd91323fb5270fa8f75f4ac67601c878026aea
chrisewoi <69186935+chrisewoi@users.noreply.github.com>
2026-02-08 02:07:54 +1100

    guard and mayor talk animations

--- Assets/Scripts/PortraitTalk.cs
+++ Assets/Scripts/PortraitTalk.cs
@@ -1,3 +1,4 @@
+using System.Timers;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,17 +8,31 @@ public class PortraitTalk : MonoBehaviour
     public Sprite idle;
     public Sprite talking;
     private bool wasTalking;
-    
+    [Header("How long the character takes while talking to change to their talking sprite.")]
+    [Header("Adds a bit of personality.")]
+    [Header("Don't go below 0.1f or it glitches a lil")]
+    public float timeToTalk;
+
+    private float timer;
+    private float timer2;
     void Start()
     {
         animator = GetComponentInParent<Animator>();
+        timer = 0f;
+        timer2 = 0f;
     }
 
     void Update()
     {
         bool isTalking = UIManager.Ins.GetTextAnimating();
         if(UIManager.Ins.mainText == "")isTalking = false; // because textAnimating is true during question list for some reason...
+        timer += Time.deltaTime;
+        timer2 += Time.deltaTime;
+        if (!isTalking) timer = 0f;
+        if (isTalking) timer2 = 0f;
         if (isTalking == wasTalking) return;
+        if (isTalking && timer < timeToTalk * Random.Range(1f,1.5f)) return;
+        if(!isTalking && timer2 < timeToTalk * Random.Range(1f,1.5f)) return;
         animator.SetBool("isTalking", isTalking);//image.sprite = isTalking? talking : idle;
         wasTalking = isTalking;
     }
