45f4256c92dde5cb5bd63012b773c0fe8edc7aa4
Chumplumps <47583539+Chumplumps@users.noreply.github.com>
2026-07-30 13:22:41 +1000

    Menu transition

--- Assets/Scripts/IntroManager.cs
+++ Assets/Scripts/IntroManager.cs
@@ -1,16 +1,33 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class IntroManager : MonoBehaviour
 {
-    [Range(60,120)]public int targetFrameRate = 60;
+    public Animator anim;
+    [Range(60, 120)] public int targetFrameRate = 60;
     public string gameSceneName = "ChrisUI";
     void Start()
     {
         Application.targetFrameRate = targetFrameRate;
     }
 
-    public void LoadNewGame()
+    public void NewGame()
     {
-        Application.LoadLevel(gameSceneName);
+        StartCoroutine(BeginTransition());
     }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    IEnumerator BeginTransition()
+    {
+        anim.Play("Begin");
+        yield return new WaitForSeconds(6.7f);
+        SceneManager.LoadScene(gameSceneName);
+    }
+
+
 }
